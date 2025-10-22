9ca6153a71e5e235c7fdbf64d877472a80f3d678
sibamshah <tskshivam13@gmail.com>
2025-10-22 23:17:55 +0530

    Create intro.c

---
+++ Lab_1/intro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Name: Sibam Kumar Shah\n");
+printf("Roll Number: MEB25020\n");
+printf("Branch: Mechanical Engineering\n");
+printf("University: Tezpur University\n");
+}
