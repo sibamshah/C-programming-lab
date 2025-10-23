c67d4fa2aacae94900d7298df5ebc941e320ad99
sibamshah <tskshivam13@gmail.com>
2025-10-23 22:39:21 +0530

    Create sumoftwonumbers.c

---
+++ Lab_2/sumoftwonumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter the first number:");
+    scanf("%d" ,&a);
+    printf("enter the second number:");
+    scanf("%d" ,&b);
+    //logic to find the sum
+    sum=a+b;
+    //the result
+    printf("the sum of the two numbers is:%d",sum);
+    return 0;
+}
