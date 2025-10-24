d3002bf0f471f26824182d5f2d46fa541ea51ed0
sibamshah <tskshivam13@gmail.com>
2025-10-24 17:14:11 +0530

    Update temperature_check_system.c

--- Lab_3/temperature_check_system.c
+++ Lab_3/temperature_check_system.c
@@ -4,6 +4,7 @@ int main()
 	int a;
 	printf("enter the temperature in degree celcius");
 	scanf("%d",&a);
+	//checking if the number is in the range or not
 	if(a>60 && a<=80)
 	{
 		printf("safe range");
